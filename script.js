b9d7d16fd54a97897625eeca1912f1f79d5485f4
artur <arturaguilar426@gmail.com>
2025-02-09 19:14:40 -0300

    ATUALIZAÇÂO

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const menu = document.querySelector('.menu');
-        const NavMenu = document.querySelector('.nav-menu');
+const menu = document.querySelector('.menu'); // pega o elemento com a classe menu 
+const NavMenu = document.querySelector('.nav-menu'); // pega o elemento com a classe nav-menu
 
-        menu.addEventListener('click', () => {
-            menu.classList.toggle('ativo');
-            NavMenu.classList.toggle('ativo');
+        menu.addEventListener('click', () => { // adiciona um evento de click no elemento menu
+            menu.classList.toggle('ativo'); // adiciona a classe ativo ao elemento menu
+            NavMenu.classList.toggle('ativo'); // adiciona a classe ativo ao elemento nav-menu
         });
 
-        function fecharMenu() {
-            document.querySelector(".nav-menu").classList.remove("ativo");
-            document.querySelector(".menu").classList.remove("ativo");
+        function fecharMenu() { // função para fechar o menu
+            document.querySelector(".nav-menu").classList.remove("ativo"); // remove a classe ativo do elemento nav-menu
+            document.querySelector(".menu").classList.remove("ativo"); // remove a classe ativo do elemento menu
 
-            menu.addEventListener('click' , () => {
-                menu.classList.toggle('desativar');
-                NavMenu.classList.toggle('desativar');
+            menu.addEventListener('click' , () => { // adiciona um evento de click no elemento menu
+                menu.classList.toggle('desativar'); // adiciona a classe desativar ao elemento menu
+                NavMenu.classList.toggle('desativar'); // adiciona a classe desativar ao elemento nav-menu
                 });
 }
